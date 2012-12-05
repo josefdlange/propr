84bed94459a7e88507b0aaf7c0fd69f7243833d7
Nick Burns <nbburns@ups.edu>
2012-12-05 02:11:23 -0800

    data generator working

--- sql/data-generator/data-gen.py
+++ sql/data-generator/data-gen.py
@@ -3,20 +3,47 @@ import ConfigParser, json, os, random
 
 def main():
     config = ConfigParser.ConfigParser()
-    config.readfp(open('data.cfg'))
+    config.readfp(open('data-gen.cfg'))
     tables = []
     for section in config.sections():
         newTable = {}
+        newTable = []
+        length = 0
+        nullCol = None
         for option in config.options(section):
             values = config.get(section, option)
-            values = values.replace(' ','')
+            values = values.replace(', ',',')
             values = values.split(',')
-            if len(values) == 1:
-                newTable['length'] = int(values[0])
+            if option == 'numberofrows':
+                length = int(values[0])
             else:
-                newTable[option] = values
-        tables.append((section,newTable))
-    print tables
+                newTable.append(values)
+        tables.append((section, length, newTable))
+
+    sqlTable = ''
+    for table in tables:
+        tableName = table[0]
+        tableLength = table[1]
+        columns = table[2]
+        numColumns = len(columns)
+        sqlTable += 'INSERT INTO `' + tableName + '` VALUES\n'
+        for row in range(0,tableLength):
+            currentRow = '( '
+            for column in range(0, numColumns):
+                choice = random.choice(columns[column])
+                if choice == 'NULL':
+                    currentRow += 'NULL, '
+                else:
+                    if column == numColumns-1:
+                        currentRow += "'" + choice + "' )\n"
+                    else:
+                        currentRow += "'" + choice + "', "
+
+            sqlTable += currentRow
+        sqlTable += '\n'
+    sqlFile = open('sql-insert-data.sql', 'w+')
+    sqlFile.write(sqlTable)
+    sqlFile.close()
 
 
 if __name__ == "__main__":
